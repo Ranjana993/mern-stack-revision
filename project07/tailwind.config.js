8c31f346163b295956111b4c3418fb0de65159ed
Kumari Ranjana Yadav <yranjana757@gmail.com>
2024-12-17 13:49:08 +0530

    paytm project is in progress

---
+++ project07/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        lightBlue:'#00BAF2',
+        darkBlue:'#0F4A8A',
+        shadowlightblue:'#00AFE3',
+        darkShadowblue:'#0D3E80',
+        btnColor:'#003C81'
+      }
+    },
+  },
+  plugins: [],
+}
